ccd914abcf6c9c4bd96747f291819c2199d1334f
jetong <jose.e.tong@gmail.com>
2017-08-08 01:37:48 -0700

    python script

---
+++ random_msg.py
@@ -0,0 +1,15 @@
+import random
+
+print 'Sample python script'
+vowels=("a", "e", "i", "o", "u")
+adjectives=("amazing", "awesome", "super", "incredible", "magnificient", "excellent", "fabulous", "brilliant", "outstanding")
+
+adj=random.choice(adjectives)
+
+if(adj[0] in vowels):
+	preposition="an"
+else:
+	preposition="a"
+
+
+print("Have " + preposition + " " + adj + " day!")
